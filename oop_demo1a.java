45eed4cb8c28ee6fd900e172f9ef4e1c57390513
Jevgeni <67290830+Jevgeni-i-am@users.noreply.github.com>
2023-02-06 16:48:40 +0200

    Create oop_demo1a.java

---
+++ oop_demo1a.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class oop_demo1a {
+
+}
+
+class Artist {
+      private String artistName;
+      private String performanceGenre;
+      private int performanceFee;
+      private ArrayList gigCelendar;
+
+}
+
+class Customer {
+      private String customerName;
+      private boolean isLogged;
+}
+
+class Event {
+      private String eventName;
+      private String eventType;
+      private Artist bookedArtist;
+
+}
